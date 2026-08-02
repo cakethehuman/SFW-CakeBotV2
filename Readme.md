befc1da9d3e6232b5a165df14986455e4e7bf08c
cakethehuman <willsonputra17@gmail.com>
2026-08-02 18:53:04 +0700

    Added Basic template files

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# CakeBotv2
+
+Cake bot v2 is a upgrade of the old cake bot
---
+++ client.py
@@ -0,0 +1,7 @@
+import logging
+from pathlib import Path
+
+import discord
+from discord.ext import commands
+
+from bot.core.config import settings
