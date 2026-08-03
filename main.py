a28f87d80a2d76e509a070be05d13388e3a4fef8
Kino7916 <andrewsmite132@gmail.com>
2026-08-03 17:18:20 +0700

    zed's ide upset, so made the token field optional

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from bot.client import CakeBot
 from bot.core.config import settings
 from bot.core.logging import setup_logging
 
+
 async def main():
     setup_logging()
+    if settings.TOKEN is None:
+        raise Exception("Invalid Token")
     bot = CakeBot()
     async with bot:
         await bot.start(settings.TOKEN)
