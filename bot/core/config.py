582944789b5192ce3c9b78d8e2100d085f77e5fa
Kino7916 <andrewsmite132@gmail.com>
2026-08-03 09:36:18 +0700

    Zed doesn't like non-optional params, duno why

--- bot/core/config.py
+++ bot/core/config.py
@@ -1,10 +1,9 @@
-from typing import Optional
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
-    TOKEN: str
-    DEV_GUILD_ID: Optional[int] = None
+    TOKEN: str | None = None
+    DEV_GUILD_ID: int | None = None
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
